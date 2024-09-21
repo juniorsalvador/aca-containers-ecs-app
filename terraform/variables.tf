93590ceabac73fa66921cb3a53cfcafcd1ebeac0
Luiz Junior <junioronliner@gmail.com>
2024-09-21 16:05:35 -0300

    feat.: ajustes com os 3 tipos de autoscaling :fire:

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,14 +20,65 @@ variable "ssm_private_subnet_2" {}
 
 variable "ssm_private_subnet_3" {}
 
+variable "ssm_alb" {}
+
 variable "environment_variables" {}
 
 variable "capabilities" {}
 
 variable "service_healthcheck" {}
 
-variable "service_launch_type" {}
+variable "service_launch_type" {
+  type = list(object({
+    capacity_provider = string
+    weight            = number
+  }))
+}
 
 variable "service_task_count" {}
 
 variable "service_hosts" {}
+
+variable "scale_type" {}
+
+variable "task_minimum" {}
+
+variable "task_maximum" {}
+
+### Autoscaling de CPU
+
+variable "scale_out_cpu_threshold" {}
+
+variable "scale_out_adjustment" {}
+
+variable "scale_out_comparison_operator" {}
+
+variable "scale_out_statistic" {}
+
+variable "scale_out_period" {}
+
+variable "scale_out_evaluation_periods" {}
+
+variable "scale_out_cooldown" {}
+
+variable "scale_in_cpu_threshold" {}
+
+variable "scale_in_adjustment" {}
+
+variable "scale_in_comparison_operator" {}
+
+variable "scale_in_statistic" {}
+
+variable "scale_in_period" {}
+
+variable "scale_in_evaluation_periods" {}
+
+variable "scale_in_cooldown" {}
+
+### Tracking CPU
+
+variable "scale_tracking_cpu" {}
+
+### Tracking Requests
+
+variable "scale_tracking_requests" {}
